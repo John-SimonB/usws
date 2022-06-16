a8416f6805c7b27f98882b0a90b7ab6a710ed2a5
John-SimonB <72495440+John-SimonB@users.noreply.github.com>
2022-06-16 17:11:40 +0200

    First push, with index.html

---
+++ Webpage/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+app.secret_key = "7aw8ezfhdisuzhf"
+
+@app.route("/hello")
+def index():
+    flash("whats your name?")
+    return render_template("index.html")
+
+if __name__ == "__main__":
+            app.run()
