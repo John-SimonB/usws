ed9220f871db5802cb4a084445797dcd92a90909
John-SimonB <72495440+John-SimonB@users.noreply.github.com>
2022-06-16 17:27:23 +0200

    change for heroku

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, flash
 app = Flask(__name__)
 app.secret_key = "7aw8ezfhdisuzhf"
 
-@app.route("/hello")
+@app.route("/")
 def index():
     flash("whats your name?")
     return render_template("index.html")
