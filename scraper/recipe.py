3f66174e77b7ee9ec856c0f8d7a937f14ccd0841
John-SimonB <72495440+John-SimonB@users.noreply.github.com>
2022-06-20 11:54:32 +0200

    First recipe

--- scraper/recipe.py
+++ scraper/recipe.py
@@ -4,20 +4,33 @@ import numpy as np
 import time
 
 def open_file(name, datum):
-    
+    owd = os.getcwd()
     if os.path.exists("scraper/" + datum):
         os.chdir("scraper/" + datum)
-        print("Directory changed")
-    else:
-        print("Directory not found")
-    if os.path.isfile(name + '.csv'):
-        print("Datei gefunden!")
-        data = []
-        for chunk in pd.read_csv(name + ".csv", chunksize=10, error_bad_lines=False):
-            data.append(chunk)
-        print(data)
-    else:
-        print("keine Datei vorhanden")
+        if os.path.isfile(name + '.csv'):
+            print("File found!")
+            data = []
+            for chunk in pd.read_csv(name + ".csv", chunksize=10, error_bad_lines=False):
+                data.append(chunk)
+                os.chdir(owd)
+            return data
+        else: print("Directory not found")
+    else: print("No file found")
 
-open_file("angebote", "20.06")
+
+def read_recipe():
+    owd = os.getcwd()
+    if os.path.exists("scraper/"):
+        os.chdir("scraper/")
+        if os.path.isfile("Rezepte.xlsx"):
+            data = pd.read_excel("Rezepte.xlsx")
+            print(data)
+            os.chdir(owd)
+            return data
+        else: print("No file found")
+    else: print("Path not found")
+
+
+print(open_file("Bier", "20.06"))
+print(read_recipe())
 
