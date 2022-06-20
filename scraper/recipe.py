426634948001285fee6d44642251c21306037a04
John-SimonB <72495440+John-SimonB@users.noreply.github.com>
2022-06-20 11:25:06 +0200

    new scrape data

--- scraper/recipe.py
+++ scraper/recipe.py
@@ -1,5 +1,7 @@
 import os
 import pandas as pd
+import numpy as np
+import time
 
 def open_file(name, datum):
     
@@ -10,12 +12,12 @@ def open_file(name, datum):
         print("Directory not found")
     if os.path.isfile(name + '.csv'):
         print("Datei gefunden!")
-
-        for chunk in pd.read_csv(name + ".csv", chunksize=5):
-            print(chunk)
+        data = []
+        for chunk in pd.read_csv(name + ".csv", chunksize=10, error_bad_lines=False):
+            data.append(chunk)
+        print(data)
     else:
         print("keine Datei vorhanden")
 
-open_file("angebote", "09.06")
-
+open_file("angebote", "20.06")
 
