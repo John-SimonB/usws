f1752b2606b0346edc2651f63d514f56353c0c3e
John-SimonB <72495440+John-SimonB@users.noreply.github.com>
2022-07-01 15:02:46 +0200

    edit scraper for angebote.csv

--- scraper/Flink_Scrape.py
+++ scraper/Flink_Scrape.py
@@ -1,47 +1,42 @@
-"""
-Created on Mon May 16 17:05:14 2022
-
-@author: john-simonbachhuber
-"""
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
 
 angebote = 'https://www.goflink.com/de-DE/shop/category/angebote/'
-neu = 'https://www.goflink.com/de-DE/shop/category/de-neu/'
-frischundfertig  = 'https://www.goflink.com/de-DE/shop/category/frisch-fertig/'
-obstundgemuese = 'https://www.goflink.com/de-DE/shop/category/obst-gemuese/'
-fleischundfisch = 'https://www.goflink.com/de-DE/shop/category/fleisch-fisch/'
-grillen = 'https://www.goflink.com/de-DE/shop/category/grillen/'
-backwaren = 'https://www.goflink.com/de-DE/shop/category/backwaren/'
-eierundmilch ='https://www.goflink.com/de-DE/shop/category/eier-milch/'
-joghurtunddesserts = 'https://www.goflink.com/de-DE/shop/category/joghurt-desserts/'
-kaese = 'https://www.goflink.com/de-DE/shop/category/kaese/'
-wurstundaufschnitt = 'https://www.goflink.com/de-DE/shop/category/wurst-aufschnitt/'
-alkoholfreiegetraenke = 'https://www.goflink.com/de-DE/shop/category/alkoholfreie-getraenke/'
-bier = 'https://www.goflink.com/de-DE/shop/category/bier/'
-weinundsekt = 'https://www.goflink.com/de-DE/shop/category/wein-sekt/'
-spirituosen = 'https://www.goflink.com/de-DE/shop/category/spirituosen/'
-sweetsnacks = 'https://www.goflink.com/de-DE/shop/category/suesse-snacks/'
-salzigesnacks = 'https://www.goflink.com/de-DE/shop/category/salzige-snacks/'
-tiefkuehl = 'https://www.goflink.com/de-DE/shop/category/tiefkuehl/'
-eis = 'https://www.goflink.com/de-DE/shop/category/eis/'
-veggieundvegan = 'https://www.goflink.com/de-DE/shop/category/veggie/'
-konservenundfertiggerichte = 'https://www.goflink.com/de-DE/shop/category/konserven-und-fertiggerichte/'
-saucenoeleundgewuerze = 'https://www.goflink.com/de-DE/shop/category/saucen-oele-gewuerze/'
-aufstrichundcerealien = 'https://www.goflink.com/de-DE/shop/category/aufstrich-cerealien/'
-nudelnreisgetreide = 'https://www.goflink.com/de-DE/shop/category/nudeln-reis-getreide/'
-kaffeteekakao = 'https://www.goflink.com/de-DE/shop/category/kaffee-tee-kakao/'
-backenunddesserts = 'https://www.goflink.com/de-DE/shop/category/backen-dessert/'
-internationalekueche = 'https://www.goflink.com/de-DE/shop/category/internationale-kueche/'
+# neu = 'https://www.goflink.com/de-DE/shop/category/de-neu/'
+# frischundfertig  = 'https://www.goflink.com/de-DE/shop/category/frisch-fertig/'
+# obstundgemuese = 'https://www.goflink.com/de-DE/shop/category/obst-gemuese/'
+# fleischundfisch = 'https://www.goflink.com/de-DE/shop/category/fleisch-fisch/'
+# grillen = 'https://www.goflink.com/de-DE/shop/category/grillen/'
+# backwaren = 'https://www.goflink.com/de-DE/shop/category/backwaren/'
+# eierundmilch ='https://www.goflink.com/de-DE/shop/category/eier-milch/'
+# joghurtunddesserts = 'https://www.goflink.com/de-DE/shop/category/joghurt-desserts/'
+# kaese = 'https://www.goflink.com/de-DE/shop/category/kaese/'
+# wurstundaufschnitt = 'https://www.goflink.com/de-DE/shop/category/wurst-aufschnitt/'
+# alkoholfreiegetraenke = 'https://www.goflink.com/de-DE/shop/category/alkoholfreie-getraenke/'
+# bier = 'https://www.goflink.com/de-DE/shop/category/bier/'
+# weinundsekt = 'https://www.goflink.com/de-DE/shop/category/wein-sekt/'
+# spirituosen = 'https://www.goflink.com/de-DE/shop/category/spirituosen/'
+# sweetsnacks = 'https://www.goflink.com/de-DE/shop/category/suesse-snacks/'
+# salzigesnacks = 'https://www.goflink.com/de-DE/shop/category/salzige-snacks/'
+# tiefkuehl = 'https://www.goflink.com/de-DE/shop/category/tiefkuehl/'
+# eis = 'https://www.goflink.com/de-DE/shop/category/eis/'
+# veggieundvegan = 'https://www.goflink.com/de-DE/shop/category/veggie/'
+# konservenundfertiggerichte = 'https://www.goflink.com/de-DE/shop/category/konserven-und-fertiggerichte/'
+# saucenoeleundgewuerze = 'https://www.goflink.com/de-DE/shop/category/saucen-oele-gewuerze/'
+# aufstrichundcerealien = 'https://www.goflink.com/de-DE/shop/category/aufstrich-cerealien/'
+# nudelnreisgetreide = 'https://www.goflink.com/de-DE/shop/category/nudeln-reis-getreide/'
+# kaffeteekakao = 'https://www.goflink.com/de-DE/shop/category/kaffee-tee-kakao/'
+# backenunddesserts = 'https://www.goflink.com/de-DE/shop/category/backen-dessert/'
+# internationalekueche = 'https://www.goflink.com/de-DE/shop/category/internationale-kueche/'
     
-drogerieundhygiene = 'https://www.goflink.com/de-DE/shop/category/drogerie-hygiene/'
-kosmetik = 'https://www.goflink.com/de-DE/shop/category/kosmetik/'
-haushaltundtechnik = 'https://www.goflink.com/de-DE/shop/category/kueche-haushalt-technik/'
-baby = 'https://www.goflink.com/de-DE/shop/category/baby-titel/'
-tierfutter = 'https://www.goflink.com/de-DE/shop/category/tierfutter/'
-fitness ='https://www.goflink.com/de-DE/shop/category/fitness/'
+# drogerieundhygiene = 'https://www.goflink.com/de-DE/shop/category/drogerie-hygiene/'
+# kosmetik = 'https://www.goflink.com/de-DE/shop/category/kosmetik/'
+# haushaltundtechnik = 'https://www.goflink.com/de-DE/shop/category/kueche-haushalt-technik/'
+# baby = 'https://www.goflink.com/de-DE/shop/category/baby-titel/'
+# tierfutter = 'https://www.goflink.com/de-DE/shop/category/tierfutter/'
+# fitness ='https://www.goflink.com/de-DE/shop/category/fitness/'
 
 def get_page(url):
     response = requests.get(url)
@@ -97,42 +92,41 @@ def output(productlist, file_name):
     return
 
 
-#wenn möglich bitte diese zeilen gegen einen loop ersetzen =)
 output(parse(get_page(angebote)), "angebote")
-output(parse(get_page(neu)), "neu")
-output(parse(get_page(frischundfertig)), "frischundfertig")
-output(parse(get_page(obstundgemuese)), "obstundgemuese")
-output(parse(get_page(fleischundfisch)), "fleischundfisch")
-output(parse(get_page(grillen)), "grillen")
-output(parse(get_page(backwaren)), "backwaren")
-output(parse(get_page(eierundmilch)), "eierundmilch")
-output(parse(get_page(joghurtunddesserts)), "joghurtunddesserts")
-output(parse(get_page(kaese)), "kaese")
-output(parse(get_page(wurstundaufschnitt)), "wurstundaufschnitt")
-output(parse(get_page(alkoholfreiegetraenke)), "alkoholfreiegetraenke")
-output(parse(get_page(bier)), "bier")
-output(parse(get_page(weinundsekt)), "weinundsekt")
-output(parse(get_page(spirituosen)), "spirituosen")
-output(parse(get_page(sweetsnacks)), "sweetsnacks")
-print("\n\n\n\n\n\n\n\n50% finshed!\n\n\n\n\n\n\n\n")
-output(parse(get_page(salzigesnacks)), "salzigesnacks")
-output(parse(get_page(tiefkuehl)), "tiefkuehl")
-output(parse(get_page(eis)), "eis")
-output(parse(get_page(veggieundvegan)), "veggieundvegan")
-output(parse(get_page(konservenundfertiggerichte)), "konservenundfertiggerichte")
-output(parse(get_page(saucenoeleundgewuerze)), "saucenoeleundgewuerze")
-output(parse(get_page(aufstrichundcerealien)), "aufstrichundcerealien")
-output(parse(get_page(nudelnreisgetreide)), "nudelnreisgetreide")
-output(parse(get_page(kaffeteekakao)), "kaffeeteekakao")
-output(parse(get_page(backenunddesserts)), "backenunddesserts")
-output(parse(get_page(internationalekueche)), "internationalekueche")
+# output(parse(get_page(neu)), "neu")
+# output(parse(get_page(frischundfertig)), "frischundfertig")
+# output(parse(get_page(obstundgemuese)), "obstundgemuese")
+# output(parse(get_page(fleischundfisch)), "fleischundfisch")
+# output(parse(get_page(grillen)), "grillen")
+# output(parse(get_page(backwaren)), "backwaren")
+# output(parse(get_page(eierundmilch)), "eierundmilch")
+# output(parse(get_page(joghurtunddesserts)), "joghurtunddesserts")
+# output(parse(get_page(kaese)), "kaese")
+# output(parse(get_page(wurstundaufschnitt)), "wurstundaufschnitt")
+# output(parse(get_page(alkoholfreiegetraenke)), "alkoholfreiegetraenke")
+# output(parse(get_page(bier)), "bier")
+# output(parse(get_page(weinundsekt)), "weinundsekt")
+# output(parse(get_page(spirituosen)), "spirituosen")
+# output(parse(get_page(sweetsnacks)), "sweetsnacks")
+# print("\n\n\n\n\n\n\n\n50% finshed!\n\n\n\n\n\n\n\n")
+# output(parse(get_page(salzigesnacks)), "salzigesnacks")
+# output(parse(get_page(tiefkuehl)), "tiefkuehl")
+# output(parse(get_page(eis)), "eis")
+# output(parse(get_page(veggieundvegan)), "veggieundvegan")
+# output(parse(get_page(konservenundfertiggerichte)), "konservenundfertiggerichte")
+# output(parse(get_page(saucenoeleundgewuerze)), "saucenoeleundgewuerze")
+# output(parse(get_page(aufstrichundcerealien)), "aufstrichundcerealien")
+# output(parse(get_page(nudelnreisgetreide)), "nudelnreisgetreide")
+# output(parse(get_page(kaffeteekakao)), "kaffeeteekakao")
+# output(parse(get_page(backenunddesserts)), "backenunddesserts")
+# output(parse(get_page(internationalekueche)), "internationalekueche")
 
-output(parse(get_page(drogerieundhygiene)), "drogerieundhygiene")
-output(parse(get_page(kosmetik)), "kosmetik")
-output(parse(get_page(haushaltundtechnik)), "haushaltundtechnik")
-output(parse(get_page(baby)), "baby")
-output(parse(get_page(tierfutter)), "tierfutter")
-output(parse(get_page(fitness)), "fitness")
+# output(parse(get_page(drogerieundhygiene)), "drogerieundhygiene")
+# output(parse(get_page(kosmetik)), "kosmetik")
+# output(parse(get_page(haushaltundtechnik)), "haushaltundtechnik")
+# output(parse(get_page(baby)), "baby")
+# output(parse(get_page(tierfutter)), "tierfutter")
+# output(parse(get_page(fitness)), "fitness")
 
 
 print("\n\n\n\n\nData succsessfully scraped!")
